4c98345a81ad20c61c5202420f23ca8a01b43698
Saiprabhu007 <saiprabhu.dandanayak@zemosolabs.com>
2024-03-18 14:27:17 +0530

    🔐 feat : added database secrets in .env

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +31,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+
+load_dotenv()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -77,11 +81,11 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-crud',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
